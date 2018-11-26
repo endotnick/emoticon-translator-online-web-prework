2834f84ca847be4a0b223b299c32a0fca1b82004
Nick Rodriguez <nickbenrodriguez@gmail.com>
2018-11-26 06:29:42 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,10 +12,10 @@ end
 
 def get_japanese_emoticon(path, emote)
   lib = load_library(path)
-  lib[get_emoticon][emote]
+  lib['get_emoticon'][emote]
 end
 
 def get_english_meaning(path, emote)
   lib = load_library(path)
-  lib[get_meaning][emote]
+  lib['get_meaning'][emote]
 end
